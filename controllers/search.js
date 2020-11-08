a456390c2079313e11286c314ed7c8f14e30ce62
Caner <caner.erdogan.92@gmail.com>
2020-11-08 21:48:21 +0300

    Quality fix

--- controllers/search.js
+++ controllers/search.js
@@ -5,10 +5,10 @@ const sendRequestToSearch = (req, res) => {
   const { site, query } = req.body;
   switch (site) {
     case "duckduckgo":
-      res.json(`https://www.duckduckgo.com/?q=${query}`)
+      res.json(`https://www.duckduckgo.com/?q=${query}`);
       break;
     case "google":
-      res.json(`https://www.google.com/search?q=${query}`)
+      res.json(`https://www.google.com/search?q=${query}`);
       break;
     default:
       res.status(400).json("Site key undefined");
