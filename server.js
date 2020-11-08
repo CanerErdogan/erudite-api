bc612e7a6cac609a8a946ee789c1fc78ea2e359d
Caner <caner.erdogan.92@gmail.com>
2020-11-08 21:49:55 +0300

    Quality fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/search', (req, res) => {search.sendRequestToSearch(req, res)});
+app.post('/search', (req, res) => { search.sendRequestToSearch(req, res) });
 
 ERUDITE_API_PORT = process.env.ERUDITE_API_PORT || 3001;
 app.listen(ERUDITE_API_PORT, () => console.log(`Listening on port ${ERUDITE_API_PORT}`));
