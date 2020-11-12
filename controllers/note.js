2e6554553fd22efb2162c952eac11ad894d990a1
Caner <caner.erdogan.92@gmail.com>
2020-11-12 19:39:20 +0300

    Edit implemented.

---
+++ controllers/note.js
@@ -0,0 +1,48 @@
+const createTimestamp = () => {
+  return new Date().toISOString().slice(0, 19).replace('T', ' ');
+};
+
+const getNote = (db) => (req, res) => {
+  const lastcall = createTimestamp();
+  const sign = req.params.id ? '=' : '>';
+  const id = req.params.id || 0;
+  db('notes').where('id', sign, id).select('*')
+    .then(data => {
+      if (data.length) {
+        note = data[Math.floor(Math.random() * data.length)];
+        db('notes').where('id', '=', note.id).increment('callcount', 1).catch(console.log);
+        db('notes').where('id', '=', note.id).update({ lastcall }).catch(console.log);
+        res.json(note);
+      }
+      else { res.sendStatus(204) }
+    }).catch(err => res.status(400).json('Unable to read notes.'));
+};
+
+const postNote = (db) => (req, res) => {
+  const { title, content } = req.body;
+  const lastcall = createTimestamp();
+  db('notes').insert({ title, content, lastcall }, ['id'])
+    .then(output => {
+      const { id } = output[0];
+      res.json(id);
+    })
+    .catch(err => res.status(400).json("Failed to append note."));
+};
+
+const putNote = (db) => (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  const lastcall = createTimestamp();
+  db('notes').where('id', '=', id).update({ title, content, lastcall })
+    .then(output => res.json(id))
+    .catch(err => res.status(400).json("Failed to update note."));
+}
+
+const deleteNote = (db) => (req, res) => {
+  const { id } = req.params;
+  db('notes').where({ id }).del()
+    .then(res.json("Note deleted successfully."))
+    .catch(err => res.status(400).json("Failed to delete note."));
+};
+
+module.exports = { getNote, postNote, putNote, deleteNote };
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex');
+
+const { PSQL_USER, PSQL_PASSWORD } = require('./keys');
 
 const search = require('./controllers/search');
 const background = require('./controllers/background');
+const note = require('./controllers/note');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: PSQL_USER,
+    password: PSQL_PASSWORD,
+    database: 'erudite'
+  }
+});
+
 app.post('/search', (req, res) => { search.sendRequestToSearch(req, res) });
-app.get('/background', (req, res) => { background.getBgImage(req, res) });
+// app.get('/background', (req, res) => { background.getBgImage(req, res) });
+app.route('/note/:id?')
+  .get(note.getNote(db))
+  .post(note.postNote(db))
+  .put(note.putNote(db))
+  .delete(note.deleteNote(db));
 
 ERUDITE_API_PORT = process.env.ERUDITE_API_PORT || 3001;
 app.listen(ERUDITE_API_PORT, () => console.log(`Listening on port ${ERUDITE_API_PORT}`));
